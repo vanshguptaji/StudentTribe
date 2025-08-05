c295ee142cd0133bb28f766e424cfcc1712aa38e
vansh mac <vansh23164033@akgec.ac.in>
2025-08-05 13:45:18 +0530

    feat: add global styles to disable image selection and dragging; refactor splash screen rendering

--- src/App.jsx
+++ src/App.jsx
@@ -49,25 +49,55 @@ function App() {
   // If splash screen should be shown, render only the splash screen
   if (showSplash) {
     return (
-      <div
-        onClick={handleSplashClick}
-        style={{
-          cursor: 'pointer',
-          width: '100vw',
-          height: '100vh',
-          userSelect: 'none',
-          WebkitUserSelect: 'none',
-          MozUserSelect: 'none',
-          msUserSelect: 'none',
-        }}
-      >
-        <SplashSplash2 fade={fadeOut} />
-      </div>
+      <>
+        {/* Disable image selection and dragging globally */}
+        <style>{`
+          img, svg {
+            user-select: none !important;
+            -webkit-user-select: none !important;
+            -moz-user-select: none !important;
+            -ms-user-select: none !important;
+            pointer-events: auto;
+          }
+          img, svg {
+            -webkit-user-drag: none !important;
+            user-drag: none !important;
+          }
+        `}</style>
+        <div
+          onClick={handleSplashClick}
+          style={{
+            cursor: 'pointer',
+            width: '100vw',
+            height: '100vh',
+            userSelect: 'none',
+            WebkitUserSelect: 'none',
+            MozUserSelect: 'none',
+            msUserSelect: 'none',
+          }}
+        >
+          <SplashSplash2 fade={fadeOut} />
+        </div>
+      </>
     );
   }
 
   return (
     <Router>
+      {/* Disable image selection and dragging globally */}
+      <style>{`
+        img, svg {
+          user-select: none !important;
+          -webkit-user-select: none !important;
+          -moz-user-select: none !important;
+          -ms-user-select: none !important;
+          pointer-events: auto;
+        }
+        img, svg {
+          -webkit-user-drag: none !important;
+          user-drag: none !important;
+        }
+      `}</style>
       <div
         className="min-h-screen w-screen bg-gradient-to-br from-[#b8001f] to-[#7a0015] overflow-hidden relative transition-colors duration-500 flex flex-col"
         style={{
